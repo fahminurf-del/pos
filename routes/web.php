4556fcf810f8fc56efc6e841096be72cbb374c2a
fahminurf-del <fahminurf@gmail.com>
2026-05-06 15:48:34 +0700

    feat(middleware) : guest amd make route middleware

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
+})->middleware('guest');
+
+Route::post('/login', [LoginController::class,'handleLogin'])->name('login')->middleware('guest');
+
+
+
+
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+    Route::post('/logout', [LoginController::class,'logout'])->name('logout');
 });
 
-Route::post('/login', [LoginController::class,'handleLogin'])->name('login');
-
-Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
