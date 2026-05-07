0392722507ce47780de5e57b61642d1b5e9642e3
fahminurf-del <fahminurf@gmail.com>
2026-05-07 15:36:44 +0700

    feat : updating kategori controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,11 +11,17 @@ Route::get('/', function () {
 
 Route::post('/login', [LoginController::class,'handleLogin'])->name('login')->middleware('guest');
 
-
-
-
 Route::middleware('auth')->group(function(){
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
     Route::post('/logout', [LoginController::class,'logout'])->name('logout');
+
+    //master-data.kategori.index
+    //master-data.kategori.index
+
+    Route::prefix('master-data')->as('master-data.')->group(function(){
+        Route::prefix('kategori')->as('kategori.')->controller(KategoriController::class)->group(function(){
+            Route::get('/','index')->name('index');
+        });
+    });
 });
 
