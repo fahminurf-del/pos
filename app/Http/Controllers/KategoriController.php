32a4a3d1490eaa1bcb4cc0306954642fe83f455d
fahminurf-del <fahminurf@gmail.com>
2026-05-07 15:31:49 +0700

    feat: make function for kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    //
+    public function index(){
+        $kategori = Kategori::all();
+        return view('kategori.index', compact('kategori'));
+    }
 }
