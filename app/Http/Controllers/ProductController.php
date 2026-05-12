c1477fd51c3d536b948aba220a5eca0e1b169e2d
fahminurf-del <fahminurf@gmail.com>
2026-05-12 22:16:01 +0700

    feat (product) : make update controller for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,8 +9,60 @@ class ProductController extends Controller
 {
     public function index(){
         $products = Product::all();
-
+        confirmDelete('Hapus Data', 'Apakah anda yakin ingin menghapus data ini?');
         return view('product.index', compact('products'));
 
+    }
+    public function store(Request $request){
+        $id = $request->id;
+        $request->validate([
+            'nama_product'     => 'required|unique:products,nama_produk,'.$id,
+            'harga_jual'       => 'required|numeric|min:0',
+            'harga_beli_pokok' => 'required|numeric|min:0',
+            'kategori_id'      => 'required|exists:kategoris,id',
+            'stok'             => 'required|numeric|min:0',
+            'stok_minimal'     => 'required|numeric|min:0',
+
+        ],[
+            'nama_product.reqiured'     => 'Nama produk harus diisi',
+            'nama_product.unique'       => 'Nama produk sudah ada',
+            'harga_jual.required'       => 'Harga jual harus diisi',
+            'harga_jual.numeric'        => 'Harga jual harus berupa angka',
+            'harga_jual.min'            => 'Harga jual minimal adalah 0',
+            'harga_beli_pokok.required' => 'Harga beli pokok harus diisi',
+            'harga_beli_pokok.numeric'  => 'Harga beli pokok harus berupa angka',
+            'harga_beli_pokok.min'      => 'Harga beli pokok minimal adalah 0',
+            'kategori_id.required'      => 'Kategori harus diisi',
+            'kategori_id.unique'        => 'Kategori tidak valid',
+            'stok.required'             => 'Stok harus diisi',
+            'stok.numeric'              => 'Stok harus berupa angka',
+        ]);
+        $newRequest = [
+            'id' => $id,
+            'nama_produk' => $request->nama_product,
+            'harga_jual' => $request->harga_jual,
+            'harga_beli_pokok' => $request->harga_beli_pokok,
+            'kategori_id' => $request->kategori_id,
+            'stok' => $request->stok,
+            'stok_minimal' => $request->stok_minimal,
+            'is_active' => $request->is_active ? true : false,
+            ];
+
+            if (!$id) {
+            $newRequest['sku'] = Product::nomorSKU();
+            }
+
+            Product::updateOrCreate(
+            ["id" => $id],
+            $newRequest
+            );
+            toast()->success('Data Berhasil Disimpan');
+            return redirect()->route('master-data.product.index');
+    }
+    public function destroy(String $id){
+        $product = Product::find($id);
+        $product->delete();
+        toast()->success('Data Berhasil Dihapus');
+        return redirect()->route('master-data.product.index');
     }
 }
