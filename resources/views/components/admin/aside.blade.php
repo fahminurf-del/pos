1d8e7a8f28d7bcd59d350ca7f918b04a49f045e1
fahminurf-del <fahminurf@gmail.com>
2026-05-22 19:17:41 +0700

    feat (dashboard) : add store logo

--- resources/views/components/admin/aside.blade.php
+++ resources/views/components/admin/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/dashboard" class="brand-link text-center">
-      {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+      <img src="{{ asset('adminlte') }}/dist/img/daster_mall.jpeg" alt="Daster Mall" class="brand-image" style="opacity: .8">
       <span class="brand-text font-weight-light"><b>{{ env('APP_NAME') }}</b></span>
     </a>
 
