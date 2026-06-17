895ec3a248b190d7f0f3d7d9c00b08bccafbda9d
fahminurf-del <fahminurf@gmail.com>
2026-06-17 17:53:57 +0700

    feat (form) : adding form input new password

---
+++ resources/views/components/user/form-ganti-password.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <!-- Button trigger modal -->
+<!-- Modal -->
+<div class="modal fade" id="formGantiPassword" tabindex="-1" aria-labelledby="formGantiPasswordLabel" aria-hidden="true">
+<form action="{{ route('users.ganti-password') }}" method="POST">
+    @csrf
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="formGantiPasswordLabel">Ganti Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="form-group my-1">
+                <label for="">Password Lama</label>
+                <input type="password" class="form-control" name="old_password" id="old_password" required>
+                @error('old_password')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="form-group my-1">
+                <label for="">Password Baru</label>
+                <input type="password" class="form-control" name="password" id="password" required>
+                @error('password')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <div class="form-group my-1">
+                <label for="">Konfirmasi Password</label>
+                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" required>
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Ganti Password</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</div>
