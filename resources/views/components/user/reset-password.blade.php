4383b51ec6b668cea18a5d20d3cb0325cccf1f9c
fahminurf-del <fahminurf@gmail.com>
2026-06-18 01:00:50 +0700

    feat (): adding reset password form

---
+++ resources/views/components/user/reset-password.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formResetPassword{{ $id }}">
+  Reset Password
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="formResetPassword{{ $id }}" tabindex="-1" aria-labelledby="formResetPasswordLabel{{ $id }}" aria-hidden="true">
+  <form action="{{ route('users.reset-password', $id) }}" method="POST">
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="formResetPasswordLabel{{ $id }}">Form Reset Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-danger">Reset Password</button>
+      </div>
+    </div>
+  </div>
+</form>
+</div>
+</div>
