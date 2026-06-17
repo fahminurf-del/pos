580ff1a49caf7e576fc90ec2e7a98a3006284870
fahminurf-del <fahminurf@gmail.com>
2026-06-17 17:52:41 +0700

    feat (users) : adding delete user data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Rules\Password;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        confirmDelete('Hapus data user ?', 'data user yang sudah dihapus tidak bisa dikembalikan'); 
+        confirmDelete('Hapus user ?', 'User yang sudah dihapus tidak bisa dikembalikan'); 
         return view('users.index', compact('users'));
     }
     
@@ -34,8 +36,44 @@ class UserController extends Controller
             $newRequest['password'] = Hash::make('12345678');
         }
         user::updateOrCreate(['id' => $id], $newRequest);
-        toast()->success('Data user berhasil disimpan');
+        toast()->success('User berhasil disimpan');
         return redirect()->route('users.index');
 
+    }
+    public function gantiPassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'password' => 'required|min:8|confirmed',
+            //  'password' => 'Password'::min(8)->letters()->numbers()->mixedCase()->symbols(),
+        ],[
+            'old_password.required' => 'Password lama harus diisi',
+            'password.required' => 'Password baru harus diisi',
+            'password.min' => 'Password baru minimal 8 karakter',
+            'password.confirmed' => 'Konfirmasi password tidak cocok',
+        ]);
+        $user = User::find(Auth::id());
+            if (!Hash::check($request->old_password, $user->password)) {
+                toast()->error('Password lama tidak sesuai');
+                return redirect()->route('dashboard');
+            }
+            $user->update([
+                'password' => Hash::make($request->password)
+            ]);
+            toast()->success('Password berhasil diubah');
+            return redirect()->route('dashboard');
+
+    }
+    public function destroy(String $id)
+    {
+        $user = User::find($id);
+
+        if(Auth::id() == $user->id){
+            toast()->error('Tidak bisa menghapus akun yang sedang login');
+            return redirect()->route('users.index');
+        }
+        $user->delete();
+        toast()->success('User berhasil dihapus');
+        return redirect()->route('users.index');
     }
 }
