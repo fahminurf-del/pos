faa43fdda586d8d18e3a5e97097f5b6235faa4f1
fahminurf-del <fahminurf@gmail.com>
2026-06-19 14:49:30 +0700

    feat (index) : adding js selected data

--- resources/views/penerimaan-barang/index.blade.php
+++ resources/views/penerimaan-barang/index.blade.php
@@ -6,9 +6,8 @@
         <h4 class="card-title">Penerimaan Barang</h4>
     </div>
     <div class="card-body">
-    <div>
+    <div class="d-flex">
         <select name="select2" id="select2" class="form-control">
-
         </select>
     </div>
     </div>
@@ -17,6 +16,7 @@
 @push('script')
 <script>
     $(document).ready(function () {
+        let selectedProduk = {};
         $('#select2').select2({
             theme:'bootstrap',
             placeholder: 'Pilih Barang...',
@@ -30,9 +30,21 @@
                 }
             },
             processResults:(data)=>{
-                console.log(data);
-            }
-        }
+                data.forEach((item) => {
+                    selectedProduk[item.id] = item;
+                })
+                return{
+                    results: data.map((item) => {
+                        return{
+                            id:item.id,
+                            text:item.nama_produk
+                        }
+                    })
+                }
+            },
+            cache:true
+        },
+        minimumInputLength:3
     })
     });
 </script>
